00f8794d36f5953adfa0189ac072503f694bdfac
Anatoly Gusev <gusev.tolia@yandex.ru>
2021-11-27 20:16:23 +0300

    Исправление бага с app_name в киви кассе

--- django_pay2/providers/qiwi_kassa/urls.py
+++ django_pay2/providers/qiwi_kassa/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = "qiwi"
+app_name = "qiwi_kassa"
 
 urlpatterns = [
     path("notify/", views.QiwiKassaNotifyView.as_view(), name="notify"),
